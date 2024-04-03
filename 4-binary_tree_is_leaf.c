52b4a809f2275b13e05a80940c62aa29d03993d3
GinnaVionette <9011@holbertonstudents.com>
2024-04-03 09:55:58 -0400

    testing checkers, added all prototypes

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * binary_tree_is_leaf - check if node is leaf
+ * @node: pointer to node to check
+ * Return: 1 if node is leaf, 0 or NULL if not
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL || node->left != NULL || node->right != NULL)
+    return (0);
+
+    return (1);
+}
