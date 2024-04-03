25a52b59e471a940f30ae8d6a9a95833e87ca2e9
GinnaVionette <9011@holbertonstudents.com>
2024-04-03 09:12:51 -0400

    fixing mistakes in header file and main file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,24 +9,26 @@
  * @parent: pointer to another node
  * @value: value to store new node
  * Return: Pointer to created node if success, or NULL if fail
- * /
- * binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
- * {
- * binary_tree_t *new;
- * 
- * if (parent == NULL)
- * return (NULL);
- * 
- * new = binary_tree_node(parent, value);
- * if (new == NULL)
- * return (NULL);
- * 
- * if (parent->right != NULL)
- * {
- * new->right = parent->right;
- * parent->right->parent = new;
- * }
- * parent->right = new;
- * 
- * return (new);
- * }
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+
+	parent->right = new;
+
+	return (new);
+}
