2baeec473a6dab11ba7c6f59651dfd8c92ae42e6
GinnaVionette <9011@holbertonstudents.com>
2024-04-03 11:21:49 -0400

    Fixing for betty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,10 +11,10 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree && func)
-    {
-        binary_tree_postorder(tree->left, func);
-        binary_tree_postorder(tree->right, func);
-        funct(tree->n);
-    }
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
 }
