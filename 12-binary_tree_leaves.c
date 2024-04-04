d0cefa2365c7facfb66b9829012a2071c1d2b7f0
GinnaVionette <9011@holbertonstudents.com>
2024-04-04 09:13:31 -0400

    pushing to check for betty and compiler

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_leaves - counts leaves in binary tree
+ * @tree: pointer to root node of tree to count leaves from
+ * Return: number of leaves on tree
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t leaves = 0;
+
+    if (tree)
+    {
+        leaves += (!tree->left && !tree->right) ? 1 : 0;
+        leaves += binary_tree_leaves(tree->left);
+        leaves += binary_tree_leaves(tree->right);
+    }
+    return (leaves);
+}
