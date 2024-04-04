3f27a061c5a17d2b61769d740267cdb7eff2f95a
GinnaVionette <9011@holbertonstudents.com>
2024-04-04 09:55:21 -0400

    pushing to check for betty and compiler

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_balance - measures balance factor of binary tree
+ * @tree: pointer to root node to measure balance factor
+ * Return: balance factor, if fail return NULL
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    if (tree)
+    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+    return (0);
+}
+
+/**
+ * binary_tree_inorder - Function that goes through a binary tree using
+ * in-order traversal.
+ *
+ * @tree: Is a pointer to the root node of the tree to traverse.
+ * @func: Is a pointer to a function call for each node.
+ *
+ * Return: Nothing.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	binary_tree_inorder(tree->left, func);
+
+	func(tree->n);
+
+	binary_tree_inorder(tree->right, func);
+}
